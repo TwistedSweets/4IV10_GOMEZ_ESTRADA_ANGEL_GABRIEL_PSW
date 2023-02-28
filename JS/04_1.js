d26226e387d69ba30f348cc6bc94d482f701aec6
TwistedSweets <agomeze2102@alumno.ipn.mx>
2023-02-28 09:04:21 -0600

    maybeeeeeeeeee

--- JS/04_1.js
+++ JS/04_1.js
@@ -1,6 +1,14 @@
-const formulario =  document.getElementById('formulario');
+/*const formulario =  document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
-function inversion(){
-    
+const alfa = Array.from(Array(26)).map((e, i) => i + 65);
+const alfabeto = alfa.map((x) => String.fromCharCode(x));
+console.log(alfabeto), si sirve*/
+
+function formulario(){
+    inversion = document.getElementById('inversion').value;
+    final = inversion * 0.02;
+    resultado = document.getElementById('resultado').value = final;
+
+    return false;
 }
