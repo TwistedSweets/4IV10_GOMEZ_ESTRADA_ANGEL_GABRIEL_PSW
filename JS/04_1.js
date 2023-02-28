cbdc166b9a7fe80381c22dff9f4ffc8d633ede66
TwistedSweets <agomeze2102@alumno.ipn.mx>
2023-02-28 06:48:52 -0600

    html_06

--- JS/04_1.js
+++ JS/04_1.js
@@ -1,16 +1,6 @@
 const formulario =  document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
-var inver = document.getElementById('inversion');
-var resul = document.getElementById('resultado');
-
-inputs.forEach((input) => {
-    input.addEventListener('keyup', (e) => {
-        console.log(e.target.value);
-    });
-});
-
-formulario.addEventListener('submit', (e) => {
-    resul.value = parseInt(inver * 0.02);
-    console.log(resul.value);
-});
+function inversion(){
+    
+}
