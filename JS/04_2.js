8b2191c426adae7fd33c4666bae20a81bb0cb529
TwistedSweets <agomeze2102@alumno.ipn.mx>
2023-02-28 20:54:01 -0600

    soytupadre

--- JS/04_2.js
+++ JS/04_2.js
@@ -5,9 +5,7 @@ const sueldo_total = document.getElementById('sueldo_total');
 
 
 function formulario(){
-    for(let i = 0; i < ventas.value; i++){
-        ventas = ventas.value;
-    }
+    ventas = ventas.value;
     sueldo_base = sueldo_base.value;
 
     comision = comision.value(ventas*.10);
@@ -15,3 +13,31 @@ function formulario(){
 
     return false;
 }
+
+// Second answer
+function secondF() {
+    let salarioV = document.getElementById("pagoV").value;
+    let lector = /\D/;
+  
+    if (lector.test(salarioV)) {
+      alert("Ingresa un salario valido");
+      return false;
+    } else {
+      salarioV = parseFloat(salarioV);
+      if (salarioV > 0 && salarioV < 999999) {
+        let pagoV = salarioV * 5;
+        let extra = 0.1 * pagoV;
+        let salarioF = pagoV + extra;
+        let impuestos = 0.12 * salarioF;
+        let total = salarioF - impuestos;
+        alert("Tu cobro mensual es de: $" + total);
+      } else {
+        alert("Ingresa un salario valido");
+        return false;
+      }
+    }
+  }
+  
+  function secondV() {
+    document.getElementById("pagoV").value = "";
+  }
